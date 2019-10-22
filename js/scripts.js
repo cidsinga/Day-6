11e5692d91ad8ff46086edac17befec1829fe75c
Student <example@example.com>
2019-10-22 10:43:07 -0700

    add buttons the change background color of page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#hidden").toggle();
-    $("#appear").toggle();
+    $("#hidden").slideToggle();
+    $("#appear").slideToggle();
   });
+
+  $("button#green").click(function(){
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  })
+
+  $("button#red").click(function(){
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  })
+
+  $("button#yellow").click(function(){
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  })
+
+  $("button#white").click(function(){
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  })
 });
